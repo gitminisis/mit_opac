f87eac0ecc943561c12d8f30f96903911fccfe39
opacteam <mike@minisisinc.com>
2019-10-30 17:23:13 -0700

    Fix UI #1

--- js/filter.js
+++ js/filter.js
@@ -1,19 +1,44 @@
 var filtersTag = document.getElementsByTagName("filter");
-var fileToAdd = "<div class='accordion' id='filterListAccordion'>"
+var fileToAdd = "<div class='accordion' id='filterListAccordion'>";
 for (var i = 0; i < filtersTag.length; i++) {
-    title =filtersTag[i].getAttribute("title");
-    fileToAdd += "<div class='card'><div class='card-header' id='heading" + i + "'><h5 class='mb-0'><button class='btn btn-link' type='button' data-toggle='collapse' data-target='#collapse" + i + "' aria-expanded='true' aria-controls='collapse" + i + "'>"+
-   title + "<span class='caret-holder'><i class='fa fa-caret-down'></i></span></button></h5></div><div id='collapse" + i + "' class='collapse' aria-labelledby='heading" + i + "' data-parent='#filterListAccordion'><div class='card-body'><ul class='list-group list-group-flush'>";
-    items = filtersTag[i].getElementsByTagName("item_group");
-    for (var j = 0; j < items.length; j++) {
-        link = items[j].getElementsByTagName("item_link")[0].childNodes[0].nodeValue;
-        frequency = items[j].getElementsByTagName("item_frequency")[0].childNodes[0].nodeValue;
-        value = items[j].getElementsByTagName("item_value")[0].childNodes[0].nodeValue;
-        var isSelected = items[j].getElementsByTagName("item_selected")[0].childNodes[0].nodeValue =="N"? "":"checked"
-        fileToAdd+="<li class='list-group-item'><a href='"+link+"'><input type='checkbox' class='filter-list-checkbox'"+isSelected+"><span class='filterValue'>"+value+" ("+frequency+")</span></a></li>"
-    }
-    fileToAdd+="</ul></div></div></div>"
-
+  title = filtersTag[i].getAttribute("title");
+  fileToAdd +=
+    "<div class='card'><div class='card-header' id='heading" +
+    i +
+    "'><h5 class='mb-0'><button class='btn btn-link' type='button' aria-expanded='true' aria-controls='collapse" +
+    i +
+    "'>" +
+    title +
+    "<span class='caret-holder'><i class='fa fa-caret-down'></i></span></button></h5></div><div id='collapse" +
+    i +
+    "' class='collapse show' aria-labelledby='heading" +
+    i +
+    "' ><div class='card-body'><ul class='list-group list-group-flush'>";
+  items = filtersTag[i].getElementsByTagName("item_group");
+  for (var j = 0; j < items.length; j++) {
+    link = items[j].getElementsByTagName("item_link")[0].childNodes[0]
+      .nodeValue;
+    frequency = items[j].getElementsByTagName("item_frequency")[0].childNodes[0]
+      .nodeValue;
+    value = items[j].getElementsByTagName("item_value")[0].childNodes[0]
+      .nodeValue;
+    var isSelected =
+      items[j].getElementsByTagName("item_selected")[0].childNodes[0]
+        .nodeValue == "N"
+        ? ""
+        : "checked";
+    fileToAdd +=
+      "<li class='list-group-item'><a href='" +
+      link +
+      "'><input type='checkbox' class='filter-list-checkbox'" +
+      isSelected +
+      "><span class='filterValue'>" +
+      value +
+      " (" +
+      frequency +
+      ")</span></a></li>";
+  }
+  fileToAdd += "</ul></div></div></div>";
 }
-fileToAdd+="</div>"
-$("#filter-list-items").html(fileToAdd)
+fileToAdd += "</div>";
+$("#filter-list-items").html(fileToAdd);
